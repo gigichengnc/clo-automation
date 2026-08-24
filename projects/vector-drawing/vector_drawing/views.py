ee928c8e5553d5c4f3122e3f815508d3767ac846
Gigi Cheng <256078623+gigichengnc@users.noreply.github.com>
2026-08-24 23:16:33 +0800

    clean back view implementation

--- projects/vector-drawing/vector_drawing/views.py
+++ projects/vector-drawing/vector_drawing/views.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from dataclasses import replace
-
 from .fashion_flat import DressSpec, build_dress_flat
 from .model import Cubic, Drawing, Line, Move, Point, SemanticPath
 
